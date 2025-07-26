b5141be7f8e9095d6c2455e5c5e8f51ae950332f
Kunsh <127657835+KunshrJain@users.noreply.github.com>
2025-07-26 23:34:54 +0530

    Update ignore.c

--- ignore.c
+++ ignore.c
@@ -10,5 +10,12 @@ int main(){
     printf("Ignore this is to increase weightage of C");
     printf("Ignore this is to increase weightage of C");
     printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
+    printf("Ignore this is to increase weightage of C");
     return 0;
 }
